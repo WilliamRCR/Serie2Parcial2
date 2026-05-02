c00a81dfd0004098c325d9fb50febb868e774cac
WilliamRCR <williamcastanazaromero2@gmail.com>
2026-05-02 16:22:15 -0600

    CI/CD

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+// Ruta principal que mostrará nuestro mensaje
+app.get('/', (req, res) => {
+  res.send(`
+    <html>
+      <body style="font-family: Arial; text-align: center; margin-top: 50px;">
+        <h1>William Ricardo Castañaza Romero</h1>
+        <p>Esta aplicación se desplegó automáticamente desde GitHub.</p>
+        <p>Versión: 1.0</p>
+      </body>
+    </html>
+  `);
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
