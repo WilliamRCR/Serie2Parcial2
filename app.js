2a6523bd3107dfdcc779dca96dd9c8cc8420752c
WilliamRCR <williamcastanazaromero2@gmail.com>
2026-05-02 16:34:14 -0600

    Cambio de titulo

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.get('/', (req, res) => {
   res.send(`
     <html>
       <body style="font-family: Arial; text-align: center; margin-top: 50px;">
-        <h1>William Ricardo Castañaza Romero</h1>
+        <h1>William Ricardo Castañaza Romero Carnet 0907-21-11816</h1>
         <p>Esta aplicación se desplegó automáticamente desde GitHub.</p>
         <p>Versión: 1.0</p>
       </body>
